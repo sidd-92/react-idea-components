123ccdde5f14312393a2382917da1695082c532f
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-11-16 11:38:14 +0530

    Init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "primereact/resources/themes/fluent-light/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "./assets/main.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
